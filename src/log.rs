2169eb250dcd3477a1aabc9b72331e0597306b43
Fedor Indutny <fedor@indutny.com>
2019-04-03 23:18:15 -0400

    log: really slow progress

--- src/log.rs
+++ src/log.rs
@@ -24,6 +24,17 @@ impl MultiLog {
     pub fn open(path: &Path, options: Options) -> Result<Self, Error> {
         Ok(MultiLog { path: path.to_path_buf(), options, logs: vec![], log_count: 0 })
     }
+
+    fn log_path(path: &Path, index: usize) -> PathBuf {
+        if index == 0 {
+            return path.to_path_buf();
+        }
+
+        let base = path.file_name()
+            .map(|val| val.to_str().expect("base to convert to String"))
+            .unwrap_or("default");
+        path.with_file_name(format!("{}.{}", base, index))
+    }
 }
 
 #[cfg(test)]
@@ -35,6 +46,10 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_should_create_log() {
+    fn it_should_construct_proper_log_path() {
+        assert_eq!(MultiLog::log_path(Path::new("/tmp/my-db"), 0).to_str().unwrap(),
+            "/tmp/my-db");
+        assert_eq!(MultiLog::log_path(Path::new("/tmp/my-db"), 42).to_str().unwrap(),
+            "/tmp/my-db.42");
     }
 }
